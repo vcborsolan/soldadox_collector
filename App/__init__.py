42818425de8da90bb33fe06b5289557907df96a6
Victor Orsolan <vorsolan@hotmail.com>
2020-07-03 15:39:59 -0300

    add sentry log config and updated marshmallow

--- App/__init__.py
+++ App/__init__.py
@@ -2,16 +2,21 @@ from flask import Flask
 from flask_migrate import Migrate
 from .model import configure as config_db
 from .serealizer import configure as config_ma
-import socket
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///home/victor/Projects/soldadox/test.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JSON_AS_ASCII'] = False
     config_db(app)
     config_ma(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///home/victor/Projects/soldadox/test.db'
+    sentry_sdk.init(
+        dsn="https://225751dc0e8e4a2095a83abccc18b3f0@o415817.ingest.sentry.io/5307474",
+        integrations=[FlaskIntegration()]
+    )
 
     Migrate(app , app.db)
 
