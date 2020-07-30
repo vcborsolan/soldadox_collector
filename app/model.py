3d182bd1122717b5aa364e251a5aad3e92029fb2
Victor Orsolan <vorsolan@hotmail.com>
2020-07-30 13:32:31 -0300

    adjust model length

--- app/model.py
+++ app/model.py
@@ -11,7 +11,7 @@ def configure(app):
 class State(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     initials = db.Column(db.String(2))
-    name = db.Column(db.String(20))
+    name = db.Column(db.String(50))
     ddds = db.relationship('Ddd' , backref='state' , lazy='dynamic')
     url = db.Column(db.String(255))
 
@@ -20,14 +20,14 @@ class State(db.Model):
 
 class Ddd(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30))
+    name = db.Column(db.String(50))
     state_id = db.Column(db.Integer , db.ForeignKey('state.id'))
     url = db.Column(db.String(255))
     regions = db.relationship('Region' , backref='ddd' , lazy='dynamic')
 
 class Region(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30))
+    name = db.Column(db.String(50))
     url = db.Column(db.String(255))
     ddd_id = db.Column(db.Integer , db.ForeignKey('ddd.id'))
    
@@ -37,11 +37,11 @@ class Ad(db.Model):
     title = db.Column(db.String(100))
     value = db.Column(db.String(20))
     publication = db.Column(db.String(20))
-    description = db.Column(db.String(200))
+    description = db.Column(db.String(1000))
     cod = db.Column(db.String(20))
     category = db.Column(db.String(20))
     state = db.Column(db.String(2))
-    region = db.Column(db.String(30))
+    region = db.Column(db.String(50))
     subregion = db.Column(db.String(50))
     url = db.Column(db.String(150))
     images = db.relationship('Image' , lazy='joined')
